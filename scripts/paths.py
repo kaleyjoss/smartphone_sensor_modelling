5f18004b21a23434835fee75e1852a9ade1d3115
Kaley Joss <josskaley@gmail.com>
2026-04-19 13:20:08 -0400

    change to paths.py to define reused directories (still string not Path objects, later could update this.) Move notebooks to pipeline folder, and rename folder structure. create params.yml and dvc.yaml, but these aren't in operation yet.

---
+++ scripts/paths.py
@@ -0,0 +1,25 @@
+# config/paths.py
+from pathlib import Path
+import os
+
+# Whole folder. Assuming paths.py is in a subfolder
+PROJ = os.path.join(os.getcwd(), "..")
+
+# Project folders
+DATA = os.path.join(PROJ, "data")
+SCRIPTS = os.path.join(PROJ, "data")
+NB = os.path.join(PROJ, "data")
+PIPE = os.path.join(PROJ, "pipeline")
+RESULTS = os.path.join(PROJ, "results")
+
+# Data folders
+RAW = os.path.join(DATA, "raw")
+DFS = os.path.join(DATA, "dfs")
+
+# Specific data files
+V1_DAY_RAW        = os.path.join(DATA, "v1_day_raw.csv")
+V1_DAY_IMPUTED    = os.path.join(DATA, "v1_day_trainval_imputed.csv")
+V2_WEEK_IMPUTED   = os.path.join(DATA, "v2_week_trainval_imputed.csv")
+
+
+
