7ecf04e5cb9dd52fd52b52dfb2b1dd697494fdb5
Kaley Joss <josskaley@gmail.com>
2026-05-28 10:56:06 -0400

    change depression cutoff to "over 10" to match clinical cutoff. Rerun clusters adn demographics. Change 'outputs' dir to 'demo' dir.

--- scripts/paths.py
+++ scripts/paths.py
@@ -10,13 +10,13 @@ DATA_FOLDER = os.path.join(PROJ, "data")
 SCRIPTS = os.path.join(PROJ, "data")
 NB = os.path.join(PROJ, "data")
 PIPE = os.path.join(PROJ, "pipeline")
-RESULTS = os.path.join(PROJ, "results")
-
+RESULTS = os.path.join(PROJ, "results","tables")
+CHARTS = os.path.join(PROJ, "results","charts")
 # Data folders
 RAW = os.path.join(DATA_FOLDER, "raw")
 DATA = os.path.join(DATA_FOLDER, "interim")
 SUB_DFS = os.path.join(DATA_FOLDER, "sub_dfs")
-OUTPUTS = os.path.join(DATA_FOLDER, "outputs")
+DEMO = os.path.join(DATA_FOLDER, "demographics")
 # Specific data files
 V1_DAY_RAW        = os.path.join(DATA, "v1_day_raw.csv")
 V1_DAY_IMPUTED    = os.path.join(DATA, "v1_day_trainval_imputed.csv")
