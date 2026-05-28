7ebfe4279247781a4f6860dd135de49aac767761
Kaley Joss <josskaley@gmail.com>
2026-05-28 16:45:07 -0400

    Write out charts from all the different model feature specifications, showing examples of subject predictions.

--- scripts/paths.py
+++ scripts/paths.py
@@ -17,6 +17,35 @@ RAW = os.path.join(DATA_FOLDER, "raw")
 DATA = os.path.join(DATA_FOLDER, "interim")
 SUB_DFS = os.path.join(DATA_FOLDER, "sub_dfs")
 DEMO = os.path.join(DATA_FOLDER, "demographics")
+
+# Results folder
+results_bysubject_ANOVA_dir = os.path.join(PROJ, "results","charts","model_performance","by_subject",'ANOVA15')
+results_bysubject_all_dir = os.path.join(PROJ, "results","charts","model_performance","by_subject",'all_features')
+results_bysubject_corr_dir = os.path.join(PROJ, "results","charts","model_performance","by_subject",'correlated')
+results_bysubject_LME10_dir = os.path.join(PROJ, "results","charts","model_performance","by_subject",'LME10')
+results_bysubject_sensor_dir = os.path.join(PROJ, "results","charts","model_performance","by_subject",'sensor')
+
+os.makedirs(results_bysubject_ANOVA_dir, exist_ok=True)
+os.makedirs(results_bysubject_all_dir, exist_ok=True)
+os.makedirs(results_bysubject_corr_dir, exist_ok=True)
+os.makedirs(results_bysubject_LME10_dir, exist_ok=True)
+os.makedirs(results_bysubject_sensor_dir, exist_ok=True)
+
+
+results_allsubs_ANOVA_dir = os.path.join(PROJ, "results","charts","model_performance","all_subs",'ANOVA15')
+results_allsubs_all_dir = os.path.join(PROJ, "results","charts","model_performance","all_subs",'all_features')
+results_allsubs_corr_dir = os.path.join(PROJ, "results","charts","model_performance","all_subs",'correlated')
+results_allsubs_LME10_dir = os.path.join(PROJ, "results","charts","model_performance","all_subs",'LME10')
+results_allsubs_sensor_dir = os.path.join(PROJ, "results","charts","model_performance","all_subs",'sensor')
+results_allsubs_slopeint_dir = os.path.join(PROJ, "results","charts","model_performance","all_subs",'slopeint')
+
+os.makedirs(results_allsubs_ANOVA_dir, exist_ok=True)
+os.makedirs(results_allsubs_all_dir, exist_ok=True)
+os.makedirs(results_allsubs_corr_dir, exist_ok=True)
+os.makedirs(results_allsubs_LME10_dir, exist_ok=True)
+os.makedirs(results_allsubs_sensor_dir, exist_ok=True)
+os.makedirs(results_allsubs_slopeint_dir, exist_ok=True)
+
 # Specific data files
 V1_DAY_RAW        = os.path.join(DATA, "v1_day_raw.csv")
 V1_DAY_IMPUTED    = os.path.join(DATA, "v1_day_trainval_imputed.csv")
